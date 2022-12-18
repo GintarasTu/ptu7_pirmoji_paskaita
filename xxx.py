57708ace762867288bc3bd4cb3eed8dd87cd864c
Gintaras Tumas <ginttv@gmail.com>
2022-12-18 08:13:33 +0200

    Antras bandymas

--- xxx.py
+++ xxx.py
@@ -8,6 +8,3 @@ uzsakymo_suma = pakuociu_kiekis * kaina
 mano_uzsakymas = 'Mano uzsakymas nr. {}. Užsakau {} pakuotes {} {}, kainuojančias {}. \nUž "viską" sumokėsiu {}.'
 print(mano_uzsakymas.format(eil_nr, pakuociu_kiekis, produktas, produkto_rusis, kaina, uzsakymo_suma))
 
-x = "Volvo, Audi, Skoda, BMW"
-y = x.rsplit(', ', 3)
-print(y)
